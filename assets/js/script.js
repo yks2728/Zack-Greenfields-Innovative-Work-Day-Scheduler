c6ac3d47f99b58d332e2e1755b5b6ee990ad250e
Zack Greenfield <zackg007@verizon.net>
2021-12-23 18:48:48 -0500

    fixed typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,29 @@ tasks = [];
 
 // load tasks
 var loadTasks = function () {
-  tasks = JSON.parse(localStorage.getItem("tasks"))
+  tasks = JSON.parse(localStorage.getItem("tasks"));
   if (!tasks) {
     tasks = {};
-  };
+  }
   printTasks(tasks);
-}
+};
 
 var printTasks = function () {
-  $.each(tasks, function(list, arr) {
-    var taskP = $("<p>").addClass("description task-content-" + list).text(arr);
+  $.each(tasks, function (list, arr) {
+    var taskP = $("<p>")
+      .addClass("description task-content-" + list)
+      .text(arr);
 
     $("task-content-" + list).replaceWith(taskP);
-  })
-}
+  });
+};
 
 var Today = moment().format("MMMM D, YYYY");
-$("#currentDay").text(Today);
+    $("#currentDay").text(Today);
 
 var hourAudit = function () {
   var currentHour = moment().hour();
+
   for (var i = 9; i < 18; i++) {
     var taskArea = $("#task-" + i);
     if (currentHour > i) {
@@ -31,38 +34,34 @@ var hourAudit = function () {
     } else if (currentHour === i) {
       $(taskArea).addClass("present");
     } else {
-      $(taskArea).addClass("future")
+      $(taskArea).addClass("future");
     }
   }
-}
+};
 
 // task update with click
 $(".taskBin").on("click", "p", function () {
   // get the textareas; current value/text
-  var text = $(this)
-        .text()
-        .trim();
-  var textInput = $("<textarea>")
-        .addClass("form-control")
-        .val(text);
+  var text = $(this).text().trim();
+  var textInput = $("<textarea>").addClass("form-control").val(text);
 
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
 });
 
 // Task needs to be updated
-$(".taskBin").on("blur", "textarea", function() {
+$(".taskBin").on("blur", "textarea", function () {
   // get the text areas; current value/text
-  var taskP = $("<p>")
-        .addClass("taskContent")
-        .text(text);
+  var text = $(this).val().trim();
+
+  var taskP = $("<p>").addClass("taskContent").text(text);
 
   // replace text area with p element
   $(this).replaceWith(taskP);
 });
 
 // save tasks
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
   var index = $("saveBtn").index(this);
   tasks[index] = $(this).parent().find(".taskContent").text();
   localStorage.setItem("tasks", JSON.stringify(tasks));
