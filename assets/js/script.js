69619a776e4b2121fb2a0e703750efafd6e68b5d
Zack Greenfield <zackg007@verizon.net>
2021-12-23 10:41:27 -0500

    added JS for past, present and future colors and gave credit where needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-// credit to https://github.com/mlportu/workday-scheduler/blob/master/assets/script.js for the function
-// color code hours
+// credit to https://github.com/mlportu/workday-scheduler/blob/master/assets/script.js for the functions
 
 tasks = [];
 
 // load tasks
 var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem)("tasks")
+    tasks = JSON.parse(localStorage.getItem("tasks"))
     if (!tasks) {
-        tasks = {}
-    } ;
+        tasks = {};
+    };
     printTasks(tasks)
 }
 
@@ -26,9 +25,8 @@ var Today = (moment().format("MMMM D, YYYY"))
 
 var hourAudit = function() {
     var currentHour= moment().hour()
-
-    for (var i = 9; i < 19; i++); {
-        var taskArea = $("#task-"+ i)
+    for (var i = 9; i < 18; i++) {
+        var taskArea = $("#task-" + i)
         if (currentHour > i) {
             $(taskArea).addClass("past");
         } else if (currentHour === i) {
@@ -39,4 +37,42 @@ var hourAudit = function() {
     }
 }
 
+// task update with click
+$(".taskBin").on("click", "p", function(){
+    // get the textareas; current value/text
+        var text= $(this)
+            .val()
+            .trim();
+        var textInput =$("<textarea>")
+            .addClass("form-control")
+            .val(text);
+        
+        $(this).replaceWith(textInput);
+        textInput.trigger("focus");
+});
+
+// Task needs to be updated
+$(".taskBin").on("blur", "textarea", function(){
+    // get the text areas; current value/text
+    var taskP = $("<p>")
+        .addClass("taskContent")
+        .text(text);
+    
+    // replace text area with p element
+    $(this).replaceWith(taskP);
+});
+
+// save tasks
+$(".saveBtn").on("click", function(){
+    var index =$("saveBtn").index(this);
+    tasks[index] = $(this).parent().find(".taskContent").text();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+});
+
+setInterval(function(){
+    hourAudit();},1000*60*60);
+
+loadTasks();
+hourAudit();
 // end of credit
+e.preventDefault();
